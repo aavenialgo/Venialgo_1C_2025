bf89c187f6da4fbd304f7c644bb8c48ac215f73b
Andres Venialgo <andresvenialgo15@gmail.com>
2025-03-20 09:27:33 -0300

    Creo proyecto guia1_ej3. Cambios menores al guia1_2

--- firmware/projects/guia1_ej3/main/guia1_ej3.c
+++ firmware/projects/guia1_ej3/main/guia1_ej3.c
@@ -1,9 +1,10 @@
-/*! @mainpage Blinking switch modified
+/*! @mainpage Template
  *
  * @section genDesc General Description
  *
- * Proyecto guia 1 ejercicio 2 (blinking switch modificado)
+ * This section describes how the program works.
  *
+ * <a href="https://drive.google.com/...">Operation Example</a>
  *
  * @section hardConn Hardware Connection
  *
@@ -16,63 +17,23 @@
  *
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
- * | 17/09/2023 | Document creation		                         |
+ * | 12/09/2023 | Document creation		                         |
  *
- * @author chui 
+ * @author Albano Peñalva (albano.penalva@uner.edu.ar)
  *
  */
 
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
-#include <stdbool.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "led.h"
-#include "switch.h"
 /*==================[macros and definitions]=================================*/
-bool led1_status = false;
-bool led2_status = false;
+
 /*==================[internal data definition]===============================*/
 
 /*==================[internal functions declaration]=========================*/
 
 /*==================[external functions definition]==========================*/
 void app_main(void){
-	uint8_t teclas;
-	LedsInit();
-	SwitchesInit();
-	while (1)
-	{
-	teclas = SwitchesRead();
-	switch(teclas){
-		case SWITCH_1:
-		led1_status = !led1_status;
-		if (led1_status)
-		{
-			LedOn(LED_1);
-		}
-		else{
-			LedOff(LED_1);
-		}
-		break;
-		case SWITCH_2:
-		led2_status = !led2_status;
-		if (led2_status)
-		{
-			LedOn(LED_2);
-		}
-		else{
-			LedOff(LED_2);
-		}
-		break;
-		// case SWITCH_1 & SWITCH_2:
-		// if(led1_status==true & led2_status==true)
-		// LedOn(3);
-		// break;
-	}
-	}
-
 	printf("Hello world!\n");
 }
 /*==================[end of file]============================================*/
