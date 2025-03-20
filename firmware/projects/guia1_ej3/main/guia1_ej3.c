9879f286dbd1fb2ec860e2422f0b9b31fac5afc5
Andres Venialgo <andresvenialgo15@gmail.com>
2025-03-20 11:44:58 -0300

    Cambios menores

--- firmware/projects/guia1_ej3/main/guia1_ej3.c
+++ firmware/projects/guia1_ej3/main/guia1_ej3.c
@@ -38,33 +38,37 @@ typedef struct leds
 } my_leds; 
 
 /*==================[internal data definition]===============================*/
-#define ON = 1 ;
-#define OFF = 0 ;
-#define TOGGLE = 2 ;
+#define ON 1 
+#define OFF 0 
+#define TOGGLE 2 
 #define CONFIG_BLINK_PERIOD 1000
 /*==================[internal functions declaration]=========================*/
 void funcionLeds(my_leds *leds){
 	switch (leds->mode)
 	{
-	case ON:
+	case (ON):
 			LedOn(leds->n_led);
 		break;
-	case OFF:
+	case (OFF):
 			LedOff(leds->n_led);
 		break;
-	case TOGGLE:
-	int retardo = leds->periodo / leds->n_ciclos;
+	case (TOGGLE):
+	uint8_t retardo = leds->periodo / leds->n_ciclos;
 		for (int i = 0; i < leds->n_ciclos; i++) {
 			if (leds->n_led == 1)
 				LedToggle(leds->n_led );
-			for (int j=0; j< retardo; j++){
-				vTaskDelay(CONFIG_BLINK_PERIOD/portTICK_RATE_MS);	
+			for (int j=0; j < retardo; j++){
+				vTaskDelay(CONFIG_BLINK_PERIOD/portTICK_PERIOD_MS);	
 		}
 	}
+}
 }
 
 /*==================[external functions definition]==========================*/
 void app_main(void){
+	LedsInit();
 	printf("Hello world!\n");
+	my_leds prueba1 = {1, 3, 10, 1000};
+	funcionLeds(&prueba1);
 }
 /*==================[end of file]============================================*/
