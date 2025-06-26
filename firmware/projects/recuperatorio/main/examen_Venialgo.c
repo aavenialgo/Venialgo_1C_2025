d7b0fed453cdcb159b17f11c2fd7b44393d96b55
Andres Venialgo <andresvenialgo15@gmail.com>
2025-06-26 09:02:47 -0300

    Creacion del proyecto recuperatorio

---
+++ firmware/projects/recuperatorio/main/examen_Venialgo.c
@@ -0,0 +1,37 @@
+/*! @mainpage Recuperatorio de Examen EPROM
+ *
+ * @section genDesc General Description
+ * Consigna de Aplicación: Alimentador automático de mascotas
+ * Se pretende diseñar un dispositivo basado en la ESP-EDU que se utilizará para suministrar alimento y agua a una mascota.
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 26/06/2025 | Document creation		                         |
+ *
+ * @author andres venialgo 
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	printf("Hello world!\n");
+}
+/*==================[end of file]============================================*/
