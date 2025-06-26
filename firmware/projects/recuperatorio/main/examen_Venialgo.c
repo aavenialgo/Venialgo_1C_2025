973968e53cd6547e816985a022053888e96a9192
Andres Venialgo <andresvenialgo15@gmail.com>
2025-06-26 09:40:19 -0300

    Creacion de funciones a utilizar, y construccion de un posible main (sin terminar). Se agrego algunas macros necesarias

--- firmware/projects/recuperatorio/main/examen_Venialgo.c
+++ firmware/projects/recuperatorio/main/examen_Venialgo.c
@@ -35,8 +35,17 @@
 #include "uart_mcu.h"
 #include "analog_io_mcu.h"
 /*==================[macros and definitions]=================================*/
+/*! @brief variable que guarda el volumen de agua en el recipiente */
 int16_t volumen_agua = 0; 
+/*! @brief booleano para activar/desactivar la medicion del volumen de agua */
+bool medir_agua = true;
+/*! @brief variable que guarda el peso de la comida*/
 int16_t peso_comida = 0;
+/*! @brief booleano para activar/desactivar la medicion del peso de la comida */
+bool medir_comida = true;
+/*! @brief Variable que almacena la tecla leída. */
+uint8_t key = 0;
+
 
 #define CONFIG_MEASURE_NIVEL_AGUA 2000 // TIempo medicion del nivel de agua
 #define CONFIG_MEASURE_NIVEL_COMIDA 5000 // Tiempo medicion del nivel de comida 
@@ -47,8 +56,42 @@ TaskHandle_t task_handle_comida = NULL; // Tarea para controlar la comida
 TaskHandle_t task_handle_informar = NULL; 
 /*==================[internal functions declaration]=========================*/
 
+static void medirNivelAguaTask(void *pParameter){
+
+}
+static void medirNivelComidaTask(void *pParameter){
+	
+}
+
+static void mandarInformacionTask (void *pParameter){
+
+}
+/** @brief Función asociada a la interrupción del switch 1. corresponde a medir/no medir el volumen de agua
+ */
+void functionKey1(void *pParameter){
+	medir_agua = !medir_agua; 
+}
+/** @brief Función asociada a la interrupción del switch 2. corresponde a medir/no medir el peso de la comida
+ */
+void functionKey2(void *pParameter){
+	medir_comida = !medir_comida;
+}
+
+void inicializarPerifericos(){
+	LedInit();
+	SwitchesInit();
+	SwitchActivInt(SWITCH_1, functionKey1, NULL);
+	SwitchActivInt(SWITCH_2, functionKey2, NULL);
+	HcSr04Init(GPIO_3, GPIO_2); // Trigger y Echo
+}
 /*==================[external functions definition]==========================*/
 void app_main(void){
+	inicializarPerifericos();
+
+	xTaskCreate(medirNivelAguaTask, "Medir Nivel Agua", 2048, NULL, 5, &task_handle_agua);
+	xTaskCreate(medirNivelComidaTask, "Medir Nivel Comida", 2048, NULL, 5, &task_handle_comida);
+	xTaskCreate(mandarInformacionTask, "Informar Nivel", 2048, NULL, 5, &task_handle_informar);
+
 	
 }
 /*==================[end of file]============================================*/
